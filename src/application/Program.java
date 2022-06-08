5fd12a46afd468e570605945de5097b9d3788746
josivantarcio <josivantarcio@gmail.com>
2022-06-08 14:20:53 -0300

    position class created

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Position position = new Position(5, 4);
+		System.out.println(position);
 
 	}
 
