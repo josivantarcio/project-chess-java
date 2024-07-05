37c2d3ba52294f84a9504cf05bba6094a223fd0b
Josevan Oliveira <josivantarcio@gmail.com>
2024-07-05 12:33:56 -0300

    Update Piece.java
    documentando a classe Piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,32 +1,65 @@
 package boardgame;
 
+/**
+ * A classe abstrata Piece representa uma peça genérica de um jogo de tabuleiro.
+ * Define métodos para obter o tabuleiro em que a peça está posicionada, verificar movimentos possíveis
+ * e verificar se há algum movimento possível para a peça.
+ * 
+ * @author Josevan Oliveira
+ */
 public abstract class Piece {
-	protected Position position;
-	private Board board;
-	
-	public Piece(Board board) {
-		this.board = board;
-	}
+    protected Position position;
+    private Board board;
 
-	public Board getBoard() {
-		return board;
-	}
-	
-	public abstract boolean[][] possibleMovies();
-	
-	public boolean possibleMovie(Position position) {
-		return possibleMovies()[position.getRow()][position.getColumn()];
-	}
-	
-	public boolean isthereAnyPossibleMove() {
-		boolean[][] mat = possibleMovies();
-		for (int i = 0; i < mat.length; i++) {
-			for (int j = 0; j < mat.length; j++) {
-				if(mat[i][j]) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
+    /**
+     * Construtor que associa a peça a um tabuleiro.
+     * 
+     * @param board o tabuleiro onde a peça será colocada.
+     */
+    public Piece(Board board) {
+        this.board = board;
+    }
+
+    /**
+     * Retorna o tabuleiro onde a peça está posicionada.
+     * 
+     * @return o tabuleiro onde a peça está posicionada.
+     */
+    public Board getBoard() {
+        return board;
+    }
+
+    /**
+     * Método abstrato que retorna uma matriz booleana indicando os movimentos possíveis da peça.
+     * 
+     * @return matriz booleana representando os movimentos possíveis da peça.
+     */
+    public abstract boolean[][] possibleMoves();
+
+    /**
+     * Verifica se um determinado movimento é possível para a peça.
+     * 
+     * @param position posição para verificar o movimento.
+     * @return true se o movimento é possível, false caso contrário.
+     */
+    public boolean possibleMove(Position position) {
+        return possibleMoves()[position.getRow()][position.getColumn()];
+    }
+
+    /**
+     * Verifica se há algum movimento possível para a peça.
+     * 
+     * @return true se há pelo menos um movimento possível, false caso contrário.
+     */
+    public boolean isThereAnyPossibleMove() {
+        boolean[][] mat = possibleMoves();
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                if (mat[i][j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
