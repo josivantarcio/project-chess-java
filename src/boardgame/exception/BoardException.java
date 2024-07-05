ce7fe596e41394f585b89251594df79361b59fec
Josevan Oliveira <josivantarcio@gmail.com>
2024-07-05 12:37:35 -0300

    Update BoardException.java
    documentando a unica classe de tratamento.

--- src/boardgame/exception/BoardException.java
+++ src/boardgame/exception/BoardException.java
@@ -1,10 +1,20 @@
 package boardgame.exception;
 
-public class BoardException extends RuntimeException{
-	private static final long serialVersionUID = 1L;
-	
-	public BoardException(String msg) {
-		super(msg);
-	}
+/**
+ * A classe BoardException representa uma exceção personalizada para erros relacionados ao tabuleiro do jogo.
+ * Estende a classe RuntimeException para indicar exceções que podem ocorrer durante a manipulação do tabuleiro.
+ * 
+ * @author Josevan Oliveira
+ */
+public class BoardException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
 
+    /**
+     * Construtor que recebe uma mensagem de erro para a exceção.
+     * 
+     * @param msg mensagem de erro para a exceção.
+     */
+    public BoardException(String msg) {
+        super(msg);
+    }
 }
