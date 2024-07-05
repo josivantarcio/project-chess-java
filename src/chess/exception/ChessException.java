dea7886b76291f2fc24a38e42c32306b2a549db1
Josevan Oliveira <josivantarcio@gmail.com>
2024-07-05 12:40:21 -0300

    Update ChessException.java
    documentando a classe de tratamento da pasta chess

--- src/chess/exception/ChessException.java
+++ src/chess/exception/ChessException.java
@@ -2,12 +2,21 @@ package chess.exception;
 
 import boardgame.exception.BoardException;
 
-public class ChessException extends BoardException{
-	private static final long serialVersionUID = 1L;
-	
-	public ChessException(String msg) {
-		super(msg);
-	}
+/**
+ * A classe ChessException representa uma exceção específica para erros no jogo de xadrez.
+ * Estende a classe BoardException para indicar exceções que podem ocorrer durante a execução de regras do xadrez.
+ * 
+ * @author Josevan Oliveira
+ */
+public class ChessException extends BoardException {
+    private static final long serialVersionUID = 1L;
 
-	
+    /**
+     * Construtor que recebe uma mensagem de erro para a exceção.
+     * 
+     * @param msg mensagem de erro para a exceção.
+     */
+    public ChessException(String msg) {
+        super(msg);
+    }
 }
